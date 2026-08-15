009ddf143cc1a77271a9cf56454a96998adb0b1f
Kumar_Gaurav <kgaurav3257@gmail.com>
2026-08-16 00:49:16 +0530

    Time: 19 ms (22.50%) | Memory: 31.1 MB (42.07%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -5,38 +5,18 @@ public:
         int m = matrix[0].size();
         vector<vector<int>>mat(n,vector<int>(m,0));
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(i == 0){
-                    mat[i][j] = matrix[i][j] - '0';
-                }
-                else{
-                   mat[i][j] = matrix[i][j] - '0';
-                   if(mat[i][j] == 1) mat[i][j] += mat[i-1][j];
-                }
-            }
-        }
-
-        int ans = 0;
+        int mx = 0;
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(mat[i][j]!=0){
-                    int cnt = 1;
-                    int c = j;
-                    int curr = mat[i][c];
-                    while(c<m && mat[i][c] > 0){
-                        curr = min(curr, mat[i][c]);
-                        //ans = max(ans, curr);
-                        int size = min(curr, cnt);
-                        ans =max(ans, size*size);
-                        cnt++;
-                        c++;
-                    }
+                if(matrix[i][j] == '1'){
+                    if(i == 0 || j == 0) mat[i][j] = 1;
+                    else mat[i][j] = 1 + min({mat[i-1][j], mat[i][j-1], mat[i-1][j-1]});
                 }
-            }
+                mx = max(mat[i][j], mx);
+             }
         }
 
-        return ans;
+        return mx*mx;
     }
 };
